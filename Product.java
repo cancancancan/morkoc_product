3f455c0b676b83f323b6dd915558537452080921
Morkoc <elgoog.cm@gmail.com>
2016-05-11 17:31:10 +0200

    Product Product

---
+++ Product.java
@@ -0,0 +1,20 @@
+package model.sell_morkoc;
+
+public class Product
+{
+	private String name;
+	public int price;
+	private boolean meal;
+	
+	public Product(){}
+	
+	public int getPrice()
+	{
+		return price;
+	}
+	public int setPrice()
+	{
+		this.price = price;
+	}
+	
+}
